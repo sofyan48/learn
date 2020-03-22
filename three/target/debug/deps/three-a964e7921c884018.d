8f3e8fc30c8552fea5a90fde764abceb132a34a5
sofyan48 <sofyan.saputra@ralali.com>
2020-03-22 21:43:36 +0700

    Ownership

--- three/target/debug/deps/three-a964e7921c884018.d
+++
@@ -1,5 +0,0 @@
-/Users/macbook/PROJECT/RUST/three/target/debug/deps/three-a964e7921c884018: src/main.rs
-
-/Users/macbook/PROJECT/RUST/three/target/debug/deps/three-a964e7921c884018.d: src/main.rs
-
-src/main.rs:
--- three/target/debug/three.d
+++
@@ -1,1 +0,0 @@
-/Users/macbook/PROJECT/RUST/three/target/debug/three: /Users/macbook/PROJECT/RUST/three/src/main.rs
